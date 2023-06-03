bf178faaea0fb070584af9b7d3689e08bf8e8ca3
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-06-03 12:56:41 +0100

    Add project files.

---
+++ MainComponent.h
@@ -0,0 +1,70 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent : public AudioAppComponent,
+    public ChangeListener
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent();
+
+    //==============================================================================
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    //==============================================================================
+    void paint(Graphics& g) override;
+    void resized() override;
+
+private:
+    AudioDeviceManager otherDeviceManager;
+    std::unique_ptr <AudioDeviceSelectorComponent> audioSettings;
+
+    enum TransportState
+    {
+        Stopped,
+        Starting,
+        Stopping,
+        Playing,
+        Repeating
+    };
+
+    TransportState state;
+
+    void openButtonClicked();
+    void playButtonClicked();
+    void stopButtonClicked();
+    void repeatButtonClicked();
+    void transportStateChanged(TransportState newState);
+    void changeListenerCallback(ChangeBroadcaster* source) override;
+
+    AudioFormatManager formatManager;
+    std::unique_ptr<AudioFormatReaderSource> playSource;
+    AudioTransportSource transport;
+
+    TextButton openButton;
+    TextButton playButton;
+    TextButton stopButton;
+    TextButton repeatButton;
+
+    //==============================================================================
+    // Your private member variables go here...
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
